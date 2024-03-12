3d91e0d17d5405b0de939308e2440ccca6cb2bc9
ata-kaner <ata.kaner03@gmail.com>
2024-03-12 17:03:46 +0300

    scripts: add copyright text

--- scripts/check_packages.sh
+++ scripts/check_packages.sh
@@ -1,4 +1,21 @@
-#!/bin/bash
+#!/usr/bin/env bash
+#
+# Copyright (C) 2024 npc_riddlah
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 #LOOK HERE! In order to add packages in scripts please, add it and analogues in PKG_LIST_* vars.
 source $SRC_DIR/scripts/internal/writeput.sh
 
